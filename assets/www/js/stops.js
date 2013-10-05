54c729cf1f76fcaa5d17cf7a77adb8b88eb9396d
wesley rocha <wesleyweb@gmail.com>
2013-10-05 15:20:49 -0300

    finalizando tela de listagem de pontos de ônibus

--- assets/www/js/stops.js
+++ assets/www/js/stops.js
@@ -4,27 +4,36 @@ $(function() {
 	setInterval(function() {
 		Mobee.Api.getStopsAround(updateStops);
 	}, 5000);
+
+	$("#retornar").click(function(e) {
+		window.location.href = "index.html";
+	});
+	$("#stop-item").click(function(e) {
+		window.location.href = "stops_lines.html";
+	});
 });
 
 function updateStops(data) {
-	var stopsWrapper = $(".stops");
+	var stopsWrapper = $(".stops_list");
 	
 	if (data["success"]) {
 		stopsWrapper.empty();
 		
 		var stops = data["stops"];
 		for (var index in stops) {
-			var stopDiv = $("<div></div>", {class: "stop", "data-id": stops[index].id});
+			var stopLi = $("<li></li>", {class: "stop", "data-id": stops[index].id});
+			var stopIcon = $("<span></span>", {class:"fontsmith_font onibus_ponto icone verde"});
 			var nameSpan = $("<span></span>", {class: "name"});
 			var distanceSpan = $("<span></span>", {class: "distance"});
 			
 			nameSpan.text(stops[index].name);
 			distanceSpan.text("(" + +stops[index].distance + ")");
 			
-			stopDiv.append(nameSpan);
-			stopDiv.append(distanceSpan);
+			stopLi.append(stopIcon);
+			stopLi.append(nameSpan);
+			stopLi.append(distanceSpan);
 			
-			stopsWrapper.append(stopDiv);
+			stopsWrapper.append(stopLi);
 		}
 	}
 }
